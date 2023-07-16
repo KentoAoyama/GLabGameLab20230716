a760352961e306bc146e948a2e68a757a7ca8e07
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-07-16 15:27:53 +0900

    [fix]すみません

--- Assets/Private/GoldFishController.cs
+++ Assets/Private/GoldFishController.cs
@@ -8,7 +8,12 @@ public class GoldFishController : MonoBehaviour
     private float _movePower = 1000f;
 
     [SerializeField]
+    private float _speedLimit = 10f;
+
+    [SerializeField]
     private Rigidbody2D _rb;
+
+    private float _currentMoveSpeed = 0f;
 
     void Start()
     {
@@ -17,6 +22,18 @@ public class GoldFishController : MonoBehaviour
 
     void Update()
     {
-        _rb.AddForce();
+        float deltaTime = Time.deltaTime;
+
+        Vector2 input = GetInput();
+
+        if (input.sqrMagnitude > 0)
+        {
+
+        }
+    }
+
+    private Vector2 GetInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 }
